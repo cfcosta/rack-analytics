d0f338db62bbd74afb9632c895c4934b436474fa
Cainã Costa <cainan.costa@gmail.com>
2011-01-09 18:01:12 -0200

    Created a simple redis mock for testing.

---
+++ spec/support/redis_mock.rb
@@ -0,0 +1,18 @@
+class RedisMock
+  def initialize
+    @attributes = {}
+  end
+
+  def get key
+    @attributes[key].to_s
+  end
+
+  def set key, value
+    @attributes[key] = value.to_s
+  end
+
+  def incr key
+    set key, 0 if @attributes[key].nil?
+    @atrributes[key] = (attributes[key].to_i + 1)
+  end
+end
