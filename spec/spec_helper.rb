9347489c150de7dac5571876691ab336985bdf70
Cainã Costa <cainan.costa@gmail.com>
2011-01-09 18:47:50 -0200

    Changed Webrat configuration to use the rack mode.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@ RSpec.configure do |config|
   config.include Webrat::Methods
   config.include Rack::Test::Methods
 
+  Webrat.configure do |c|
+    c.mode = :rack
+  end
+
   def app
     Rack::Builder.new do
       use Rack::Analytics::Application, :redis => db
