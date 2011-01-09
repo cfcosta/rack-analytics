7accfe4a127ad881939a322949117899057c5ae9
Cainã Costa <cainan.costa@gmail.com>
2011-01-08 22:36:32 -0200

    Added rack/test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require "webrat"
 require 'rubygems'
 require 'rspec'
 require 'webrat'
+require 'rack/test'
 
 $LOAD_PATH.unshift File.dirname(File.dirname(__FILE__)) + '/lib'
 $LOAD_PATH.unshift File.dirname(File.dirname(__FILE__))
@@ -14,4 +15,5 @@ require 'spec/support/dummy_app'
 
 RSpec.configure do |config|
   config.include Webrat::Matchers
+  config.include Rack::Test::Methods
 end
