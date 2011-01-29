d1adcfab6c050ddb397bc090daa11eb1b0b75b4f
Cainã Costa <cainan.costa@gmail.com>
2011-01-29 21:34:32 -0200

    Adjustments and removed commented test cases

--- test/request_logger_test.rb
+++ test/request_logger_test.rb
@@ -7,35 +7,35 @@ context "Rack::Analytics::RequestLogger" do
     setup { get '/' }
 
     asserts('response is ok') { last_response.ok? }
-    asserts('response has correct body') { last_response.body }.equal? "homepage"
+    asserts('response has correct body') { last_response.body }.equals "homepage"
   end
 
   context "should render a get request on a inner path correctly" do
     setup { get '/inner-page' }
 
     asserts('response is ok') { last_response.ok? }
-    asserts('response has correct body') { last_response.body }.equal? "inner page"
+    asserts('response has correct body') { last_response.body }.equals "inner page"
   end
 
   context "should render a post request correctly" do
     setup { post '/' }
 
     asserts('response is ok') { last_response.ok? }
-    asserts('response has correct body') { last_response.body }.equal? "homepage"
+    asserts('response has correct body') { last_response.body }.equals "homepage"
   end
 
   context "should render a put request correctly" do
     setup { put '/' }
 
     asserts('response is ok') { last_response.ok? }
-    asserts('response has correct body') { last_response.body }.equal? "homepage"
+    asserts('response has correct body') { last_response.body }.equals "homepage"
   end
 
   context "should render a delete request correctly" do
     setup { delete '/' }
 
     asserts('response is ok') { last_response.ok? }
-    asserts('response has correct body') { last_response.body }.equal? "homepage"
+    asserts('response has correct body') { last_response.body }.equals "homepage"
   end
 
   context "should create a access document when visiting the page" do
@@ -79,22 +79,4 @@ context "Rack::Analytics::RequestLogger" do
     asserts('it should have a referral key') { db['/'].find_one }.includes 'referral'
     asserts('it should have a correct referral set') { db['/'].find_one['referral'] }.equals 'http://www.google.com'
   end
-  # context "should save the referers informations" do
-  #   setup do
-  #     db.del "#{namespace}:/:referers"
-  #     get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
-  #   end
-  #
-  #   asserts('check referral information') { MessagePack.unpack(db.get("#{namespace}:/:referers")) }.equals({'http://www.google.com' => 1})
-  # end
-  #
-  # context "should increment the referers informations" do
-  #   setup do
-  #     db.del "#{namespace}:/:referers"
-  #     get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
-  #     get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
-  #   end
-  #
-  #   asserts('check referral information') { MessagePack.unpack(db.get("#{namespace}:/:referers")) }.equals({'http://www.google.com' => 2})
-  # end
 end
