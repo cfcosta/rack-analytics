df772be8b9711df0270b7e62cbd4efec993432b8
Cainã Costa <cainan.costa@gmail.com>
2011-01-29 15:33:10 -0200

    Changed the first test format to riot

--- test/application_test.rb
+++ test/application_test.rb
@@ -1,68 +1,68 @@
 require 'teststrap'
 
 describe Rack::Analytics::Application do
-  it "should render a get request correctly" do
-    get '/'
+  context "should render a get request correctly" do
+    setup { get '/' }
 
-    last_response.should be_ok
-    last_response.body.should == "homepage"
+    asserts('response is ok') { last_response.ok? }
+    asserts('response has correct body') { last_response.body }.equal? "homepage"
   end
 
-  it "should render a get request on a inner path correctly" do
-    get '/inner-page'
+  #it "should render a get request on a inner path correctly" do
+    #get '/inner-page'
 
-    last_response.should be_ok
-    last_response.body.should == "inner page"
-  end
+    #last_response.should be_ok
+    #last_response.body.should == "inner page"
+  #end
 
-  it "should render a post request correctly" do
-    post '/'
+  #it "should render a post request correctly" do
+    #post '/'
 
-    last_response.should be_ok
-    last_response.body.should == "homepage with post"
-  end
+    #last_response.should be_ok
+    #last_response.body.should == "homepage with post"
+  #end
 
-  it "should render a put request correctly" do
-    put '/'
+  #it "should render a put request correctly" do
+    #put '/'
 
-    last_response.should be_ok
-    last_response.body.should == "homepage with put"
-  end
+    #last_response.should be_ok
+    #last_response.body.should == "homepage with put"
+  #end
 
-  it "should render a delete request correctly" do
-    delete '/'
+  #it "should render a delete request correctly" do
+    #delete '/'
 
-    last_response.should be_ok
-    last_response.body.should == "homepage with delete"
-  end
+    #last_response.should be_ok
+    #last_response.body.should == "homepage with delete"
+  #end
 
-  it "should increment access counter of the root page" do
-    db.set("#{namespace}:/:views", 0)
+  #it "should increment access counter of the root page" do
+    #db.set("#{namespace}:/:views", 0)
 
-    get '/'
+    #get '/'
 
-    db.get("#{namespace}:/:views").should == "1"
-  end
+    #db.get("#{namespace}:/:views").should == "1"
+  #end
 
-  it "should not increment access counter on requests other than get" do
-    db.set("#{namespace}:/:views", 0)
+  #it "should not increment access counter on requests other than get" do
+    #db.set("#{namespace}:/:views", 0)
 
-    post '/'
-    put '/'
-    delete '/'
+    #post '/'
+    #put '/'
+    #delete '/'
 
-    db.get("#{namespace}:/:views").should == "0"
-  end
+    #db.get("#{namespace}:/:views").should == "0"
+  #end
 
-  it "should save the referers informations" do
-    db.del "#{namespace}:/:referers"
+  #it "should save the referers informations" do
+    #db.del "#{namespace}:/:referers"
 
-    get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
+    #get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
 
-    MessagePack.unpack(db.get("#{namespace}:/:referers")).should == {'http://www.google.com' => 1}
+    #MessagePack.unpack(db.get("#{namespace}:/:referers")).should == {'http://www.google.com' => 1}
 
-    get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
+    #get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
 
-    MessagePack.unpack(db.get("#{namespace}:/:referers")).should == {'http://www.google.com' => 2}
-  end
+    #MessagePack.unpack(db.get("#{namespace}:/:referers")).should == {'http://www.google.com' => 2}
+  #end
 end
