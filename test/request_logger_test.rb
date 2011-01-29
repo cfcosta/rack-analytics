74970c37e07f4e500f9b578ffceecde37b5805b1
Cainã Costa <cainan.costa@gmail.com>
2011-01-29 17:54:00 -0200

    Removed non implemented code and commented tests.

--- test/request_logger_test.rb
+++ test/request_logger_test.rb
@@ -35,43 +35,43 @@ describe Rack::Analytics::RequestLogger do
     asserts('response is ok') { last_response.ok? }
     asserts('response has correct body') { last_response.body }.equal? "homepage"
   end
-
-  context "should increment access count of the page" do
-    setup do
-      db.set("#{namespace}:/:views", 0)
-      get '/'
-    end
-
-    asserts('counter has incremented') { db.get("#{namespace}:/:views") }.equals "1"
-  end
-
-  context "should not increment access on put, post and delete requests" do
-    setup do
-      db.set("#{namespace}:/:views", 0)
-      post '/'
-      put '/'
-      delete '/'
-    end
-
-    asserts("counter hasn't incremented") { db.get("#{namespace}:/:views") }.equals "0"
-  end
-
-  context "should save the referers informations" do
-    setup do
-      db.del "#{namespace}:/:referers"
-      get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
-    end
-
-    asserts('check referral information') { MessagePack.unpack(db.get("#{namespace}:/:referers")) }.equals({'http://www.google.com' => 1})
-  end
-
-  context "should increment the referers informations" do
-    setup do
-      db.del "#{namespace}:/:referers"
-      get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
-      get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
-    end
-
-    asserts('check referral information') { MessagePack.unpack(db.get("#{namespace}:/:referers")) }.equals({'http://www.google.com' => 2})
-  end
+  # 
+  # context "should increment access count of the page" do
+  #   setup do
+  #     db.set("#{namespace}:/:views", 0)
+  #     get '/'
+  #   end
+  # 
+  #   asserts('counter has incremented') { db.get("#{namespace}:/:views") }.equals "1"
+  # end
+  # 
+  # context "should not increment access on put, post and delete requests" do
+  #   setup do
+  #     db.set("#{namespace}:/:views", 0)
+  #     post '/'
+  #     put '/'
+  #     delete '/'
+  #   end
+  # 
+  #   asserts("counter hasn't incremented") { db.get("#{namespace}:/:views") }.equals "0"
+  # end
+  # 
+  # context "should save the referers informations" do
+  #   setup do
+  #     db.del "#{namespace}:/:referers"
+  #     get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
+  #   end
+  # 
+  #   asserts('check referral information') { MessagePack.unpack(db.get("#{namespace}:/:referers")) }.equals({'http://www.google.com' => 1})
+  # end
+  # 
+  # context "should increment the referers informations" do
+  #   setup do
+  #     db.del "#{namespace}:/:referers"
+  #     get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
+  #     get '/', {}, 'HTTP_REFERER' => 'http://www.google.com'
+  #   end
+  # 
+  #   asserts('check referral information') { MessagePack.unpack(db.get("#{namespace}:/:referers")) }.equals({'http://www.google.com' => 2})
+  # end
 end
