a119cc80e14f599fb077af35f57834fda5e096e5
Cainã Costa <cainan.costa@gmail.com>
2011-01-30 20:04:02 -0200

    Added threading to the lib

--- lib/rack/analytics/request_logger.rb
+++ lib/rack/analytics/request_logger.rb
@@ -1,3 +1,5 @@
+require "thread"
+
 module Rack
   module Analytics
     class RequestLogger
@@ -11,15 +13,22 @@ module Rack
           access = { 'time' => Time.now }
           access['referral'] = env['HTTP_REFERER'] if env['HTTP_REFERER']
           access['user_agent'] = env['HTTP_USER_AGENT'] if env['HTTP_USER_AGENT']
-          db[env['PATH_INFO']].insert access
+
+          queue << access
+
+          Thread.new { db[env['PATH_INFO']].insert queue.pop }
         end
-        
+
         @app.call(env)
       end
 
       private
       def db
         @options[:db] || Mongo::Connection.new.db(@options[:db_name])
+      end
+      
+      def queue
+        @options[:queue] ||= Queue.new
       end
     end
   end
--- test/support/test_helpers.rb
+++ test/support/test_helpers.rb
@@ -1,7 +1,7 @@
 module TestHelpers
   def app
     Rack::Builder.new do
-      use Rack::Analytics::RequestLogger, :db => mongo
+      use Rack::Analytics::RequestLogger, :db => mongo, :queue => queue
       run DummyApp
     end
   end
@@ -9,5 +9,9 @@ module TestHelpers
   def mongo
     connection = Mongo::Connection.new
     connection.db 'rack-analytics-test'
+  end
+
+  def queue
+    Queue.new
   end
 end
