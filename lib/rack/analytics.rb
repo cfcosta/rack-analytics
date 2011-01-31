b775b60469c33c678339dd386383b9577776ec6b
Cainã Costa <cainan.costa@gmail.com>
2011-01-31 02:10:53 -0200

    Removed unnecessary changes on load paths

--- lib/rack/analytics.rb
+++ lib/rack/analytics.rb
@@ -1,6 +1,3 @@
-$LOAD_PATH.unshift File.dirname(File.dirname(__FILE__)) + '/lib'
-$LOAD_PATH.unshift File.dirname(File.dirname(__FILE__))
-
 require 'rack/analytics/request_logger'
 require 'rack/analytics/request_parser'
 
