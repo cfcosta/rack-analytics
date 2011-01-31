ce2e3c1fc0ce5cdea3673fd35d52d1e872af7c17
Cainã Costa <cainan.costa@gmail.com>
2011-01-30 23:50:59 -0200

    Added the implementation to exceptions on RequestParser

--- lib/rack/analytics/request_parser.rb
+++ lib/rack/analytics/request_parser.rb
@@ -3,17 +3,28 @@ module Rack
     class RequestParser
       attr_reader :data
 
+      DEFAULT_KEYS = ['time', 'path', 'user_agent']
+
       def initialize
       end
-      
+
+      def except=(values)
+        @except = values.to_a
+      end
+
       def parse request
         @data = {}
 
-        @data['time'] = Time.now
-        @data['path'] = request['PATH_INFO']
-        @data['user_agent'] = request['HTTP_USER_AGENT']
+        @data['time'] = Time.now if to_parse.include? 'time'
+        @data['path'] = request['PATH_INFO'] if to_parse.include? 'path'
+        @data['user_agent'] = request['HTTP_USER_AGENT'] if to_parse.include? 'user_agent'
 
         return self
+      end
+
+      private
+      def to_parse
+        @only ? @only : DEFAULT_KEYS - @except.to_a
       end
     end
   end
