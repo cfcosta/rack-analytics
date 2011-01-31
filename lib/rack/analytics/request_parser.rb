564f7566b05c338c1ec7a216c28562a1f6125fea
Cainã Costa <cainan.costa@gmail.com>
2011-01-30 23:20:23 -0200

    Added the implementation of the 'it should generate the time' test for RequestParser

--- lib/rack/analytics/request_parser.rb
+++ lib/rack/analytics/request_parser.rb
@@ -1,7 +1,18 @@
 module Rack
   module Analytics
     class RequestParser
+      attr_reader :data
+
+      def initialize
+      end
       
+      def parse request
+        @data = {}
+
+        @data['time'] = Time.now
+
+        return self
+      end
     end
   end
 end
