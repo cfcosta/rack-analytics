187ee9a1ad28075f2fa1092dce5e0abc0ab77cda
Cainã Costa <cainan.costa@gmail.com>
2011-03-04 02:48:57 -0300

    Grouped some tests to avoid context-switching

--- test/request_logger_test.rb
+++ test/request_logger_test.rb
@@ -23,6 +23,7 @@ context "Rack::Analytics::RequestLogger" do
 
     asserts('response is ok') { last_response.ok? }
     asserts('response has correct body') { last_response.body }.equals "homepage with post"
+    asserts("counter hasn't incremented") { db['views'].count }.equals 0
   end
 
   context "should render a put request correctly" do
@@ -30,6 +31,7 @@ context "Rack::Analytics::RequestLogger" do
 
     asserts('response is ok') { last_response.ok? }
     asserts('response has correct body') { last_response.body }.equals "homepage with put"
+    asserts("counter hasn't incremented") { db['views'].count }.equals 0
   end
 
   context "should render a delete request correctly" do
@@ -37,43 +39,19 @@ context "Rack::Analytics::RequestLogger" do
 
     asserts('response is ok') { last_response.ok? }
     asserts('response has correct body') { last_response.body }.equals "homepage with delete"
-  end
-
-  context "should create a access document when visiting the page" do
-    setup { get '/' }
-
-    asserts('counter has incremented') { db['views'].count }.equals 1
-  end
-
-  context "shouldn't create a access document when with post, put and delete" do
-    setup do
-      post '/'
-      put '/'
-      delete '/'
-    end
-
     asserts("counter hasn't incremented") { db['views'].count }.equals 0
   end
 
-  context "should save the path and the time of the access" do
-    setup { get '/' }
+  context "Saved information" do
+    setup { get '/', {}, 'HTTP_REFERER' => 'http://www.google.com', 'HTTP_USER_AGENT' => 'Firefox' }
 
+    asserts('counter has incremented') { db['views'].count }.equals 1
     asserts('it should have a path key') { db['views'].find_one }.includes 'path'
     asserts('it should have a path set') { db['views'].find_one['path'] }.equals '/'
     asserts('it should have a time key') { db['views'].find_one }.includes 'time'
     asserts('it should have a time set') { db['views'].find_one['time'] }.kind_of Time
-  end
-
-  context "should save the referral information" do
-    setup { get '/', {}, 'HTTP_REFERER' => 'http://www.google.com' }
-
     asserts('it should have a referral key') { db['views'].find_one }.includes 'referral'
     asserts('it should have a correct referral set') { db['views'].find_one['referral'] }.equals 'http://www.google.com'
-  end
-
-  context "should save the user agent information" do
-    setup { get 'views', {}, 'HTTP_USER_AGENT' => 'Firefox' }
-
     asserts('it should have a user agent key') { db['views'].find_one }.includes 'user_agent'
     asserts('it should have a correct user agent set') { db['views'].find_one['user_agent'] }.equals 'Firefox'
   end
